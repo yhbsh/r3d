d1e841fd83d627479e525ffb97c61babfa2aee50
yhbsh <houssembousmaha.ksu@gmail.com>
2024-11-04 15:49:16 +0300

    parse material stuff

--- tools/obj2c.c
+++ tools/obj2c.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char *argv[]) {
     if (argc != 2) {
@@ -10,26 +11,43 @@ int main(int argc, const char *argv[]) {
     char *line = NULL;
     size_t line_len = 0;
 
-    int vertices_count = 0;
-    int faces_count = 0;
-    int normals_count = 0;
-    int texture_coords_count = 0;
+    int vertices_count = 0, faces_count = 0, normals_count = 0, texture_coords_count = 0;
+    float shininess = 0.0, ambient[3] = {0.0, 0.0, 0.0}, specular[3] = {0.0, 0.0, 0.0};
+    float emissive[3] = {0.0, 0.0, 0.0}, index_of_refraction = 0.0, opacity = 1.0;
+    int illum_model = 0;
+    char texture_map[32] = {0};
 
     for (rewind(file); getline(&line, &line_len, file) != -1;) {
-        if (line[0] == 'v' && line[1] == ' ') vertices_count++;
-        if (line[0] == 'f' && line[1] == ' ') faces_count++;
-        if (line[0] == 'v' && line[1] == 'n' && line[2] == ' ') normals_count++;
-        if (line[0] == 'v' && line[1] == 't' && line[2] == ' ') texture_coords_count++;
+        if (strncmp(line, "v ", 2) == 0) vertices_count++;
+        if (strncmp(line, "vn", 2) == 0) normals_count++;
+        if (strncmp(line, "vt", 2) == 0) texture_coords_count++;
+        if (strncmp(line, "f ", 2) == 0) faces_count++;
+        if (strncmp(line, "Ns", 2) == 0) sscanf(line, "Ns %f", &shininess);
+        if (strncmp(line, "Ka", 2) == 0) sscanf(line, "Ka %f %f %f", &ambient[0], &ambient[1], &ambient[2]);
+        if (strncmp(line, "Ks", 2) == 0) sscanf(line, "Ks %f %f %f", &specular[0], &specular[1], &specular[2]);
+        if (strncmp(line, "Ke", 2) == 0) sscanf(line, "Ke %f %f %f", &emissive[0], &emissive[1], &emissive[2]);
+        if (strncmp(line, "Ni", 2) == 0) sscanf(line, "Ni %f", &index_of_refraction);
+        if (strncmp(line, "d", 1) == 0) sscanf(line, "d %f", &opacity);
+        if (strncmp(line, "illum", 5) == 0) sscanf(line, "illum %d", &illum_model);
+        if (strncmp(line, "map_Kd", 6) == 0) sscanf(line, "map_Kd %31s", texture_map);
     }
 
     printf("#define VERTICES_COUNT %d\n", vertices_count);
     printf("#define NORMALS_COUNT %d\n", normals_count);
     printf("#define TEXTURE_COORDS_COUNT %d\n", texture_coords_count);
     printf("#define FACES_COUNT %d\n", faces_count);
+    printf("#define MATERIAL_SHININESS %f\n", shininess);
+    printf("static const float MATERIAL_AMBIENT[3] = { %f, %f, %f };\n", ambient[0], ambient[1], ambient[2]);
+    printf("static const float MATERIAL_SPECULAR[3] = { %f, %f, %f };\n", specular[0], specular[1], specular[2]);
+    printf("static const float MATERIAL_EMISSIVE[3] = { %f, %f, %f };\n", emissive[0], emissive[1], emissive[2]);
+    printf("#define MATERIAL_IOR %f\n", index_of_refraction);
+    printf("#define MATERIAL_OPACITY %f\n", opacity);
+    printf("#define MATERIAL_ILLUM %d\n", illum_model);
+    printf("static const char *MATERIAL_TEXTURE_MAP = \"%s\";\n", texture_map);
 
     printf("static const float vertices[%d] = {\n", vertices_count * 3);
     for (rewind(file); getline(&line, &line_len, file) != -1;) {
-        if (line[0] != 'v' || line[1] != ' ') continue;
+        if (strncmp(line, "v ", 2) != 0) continue;
 
         float x, y, z;
         sscanf(line, "v %f %f %f", &x, &y, &z);
@@ -39,7 +57,7 @@ int main(int argc, const char *argv[]) {
 
     printf("static const float normals[%d] = {\n", normals_count * 3);
     for (rewind(file); getline(&line, &line_len, file) != -1;) {
-        if (line[0] != 'v' || line[1] != 'n' || line[2] != ' ') continue;
+        if (strncmp(line, "vn", 2) != 0) continue;
 
         float x, y, z;
         sscanf(line, "vn %f %f %f", &x, &y, &z);
@@ -49,7 +67,7 @@ int main(int argc, const char *argv[]) {
 
     printf("static const float texture_coords[%d] = {\n", texture_coords_count * 2);
     for (rewind(file); getline(&line, &line_len, file) != -1;) {
-        if (line[0] != 'v' || line[1] != 't' || line[2] != ' ') continue;
+        if (strncmp(line, "vt", 2) != 0) continue;
 
         float x, y;
         sscanf(line, "vt %f %f", &x, &y);
@@ -59,7 +77,7 @@ int main(int argc, const char *argv[]) {
 
     printf("static const unsigned int faces[%d] = {\n", faces_count * 9);
     for (rewind(file); getline(&line, &line_len, file) != -1;) {
-        if (line[0] != 'f' || line[1] != ' ') continue;
+        if (strncmp(line, "f ", 2) != 0) continue;
 
         int v_idx1 = 0, t_idx1 = 0, n_idx1 = 0;
         int v_idx2 = 0, t_idx2 = 0, n_idx2 = 0;
@@ -78,7 +96,6 @@ int main(int argc, const char *argv[]) {
         n_idx3 -= 1;
 
         printf("    %4d,%4d,%4d, %4d,%4d,%4d, %4d,%4d,%4d,\n", v_idx1, v_idx2, v_idx3, t_idx1, t_idx2, t_idx3, n_idx1, n_idx2, n_idx3);
-        // printf("    %4d,%4d,%4d,\n", v_idx1, v_idx2, v_idx3);
     }
     printf("};\n");
 
